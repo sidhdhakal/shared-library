1861f1746fcede2ce6b3b87f4878328531ca6b3e
sidhdhakal <120709779+sidhdhakal@users.noreply.github.com>
2025-06-30 17:31:00 +0545

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,12 @@
-def call(String projectName, String imageTag, String dockerhubUser) {
-    echo "Building Docker image: ${dockerhubUser}/${projectName}:${imageTag}"
-    sh "docker build -t ${dockerhubUser}/${projectName}:${imageTag} ."
+def call(Map config = [:]) {
+    def imageName = config.imageName ?: error("Image name is required")
+    def imageTag = config.imageTag ?: 'latest'
+    def dockerfile = config.dockerfile ?: 'Dockerfile'
+    def context = config.context ?: '.'
+    
+    echo "Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
+    
+    sh """
+        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
+    """
 }
